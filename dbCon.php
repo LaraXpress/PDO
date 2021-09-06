4abf4ce819305fa9a7420b808a4221a36f833629
laraxpress <mazed@isrt.ac.bd>
2021-09-07 00:40:58 +0600

    PDO : Connection Into Database updated

--- dbCon.php
+++ dbCon.php
@@ -12,3 +12,20 @@
 		exit("Error: " . $e->getMessage());
 	}
 ?>
+<!-- ANOTHER WAY CONNECTION WITH DATABASE -->
+<?php 
+	try 
+	{
+		$hostname = 'localhost';
+		$username = 'root';
+		$password = '';
+		$dbname   = 'pdoa';
+		$con      = new PDO("mysql:host=$hostname; dbname=$dbname", $username, $password);
+		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOException $e) 
+	{
+		$e->getMessage();
+		//die('Sorry!, Database is not connected properly ...!');	
+	}
+?>
